9f8cfa35802c839dc1f15a1f55916b7213d8b983
Luke Josten <josten_luke_j@lilly.com>
2017-06-23 11:39:40 -0400

    Added html files from Sierra and Courage and connected them all together

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// view engine setup and public static directory
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'lib/public')));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('sign-in', { loggedIn: false });
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('index', { loggedIn: true });
+});
+
+router.get('/fruit', function(req, res, next) {
+  res.render('fruit', { loggedIn: true });
+});
+
+router.get('/general-questions', function(req, res, next) {
+  res.render('general-questions', { loggedIn: true });
 });
 
 module.exports = router;

d8aaa6b72fb7e624d2f76ac0de28aca157cd442b
Luke Josten <josten_luke_j@lilly.com>
2017-06-25 18:00:24 -0400

    Added sign up and sign in ability - Connected the project with a real MongoDB on Heroku - Created real sign up and sign in pages - Cleaned up the routes and authenticaiton - Fixed the weird mobile responsive bug - Added a user profile page

--- app.js
+++ app.js
@@ -5,11 +5,34 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+var session = require('express-session');
+
+require('./config/passport')(passport); // pass passport for configuration
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+/* If the user is local development import the .env file, else do not load the
+.env file. Also if production is set start newrelic for monitoring*/
+if (app.get('env') === 'development') {
+  /* eslint-disable global-require */
+  require('dotenv').config();
+} else if (app.get('env') === 'production') {
+  // Import the NewRelic Module.
+  require('newrelic');
+} else {
+  console.log('Please set your NODE_ENV to either `development` or `production`');
+}
+
+mongoose.connect(process.env.MONGODB_URI); // connect to our database
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 
 // view engine setup and public static directory
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +47,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// required for passport
+app.use(session({ secret: 'py8FymaWfJShPT6Ey' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 app.use('/', index);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,68 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
 
-/* GET home page. */
+// show the signin form
 router.get('/', function(req, res, next) {
-  res.render('sign-in', { loggedIn: false });
+  res.render('sign-in', { loggedIn: false, message: req.flash('signInMessage') });
 });
 
-router.get('/home', function(req, res, next) {
-  res.render('index', { loggedIn: true });
+// process the signin form
+router.post('/sign-in', passport.authenticate('local-sign-in', {
+    successRedirect : '/home', // redirect to the secure profile section
+    failureRedirect : '/', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+}));
+
+// show the signup form
+router.get('/sign-up', function(req, res) {
+    // render the page and pass in any flash data if it exists
+    res.render('sign-up', { message: req.flash('signUpMessage') });
 });
 
-router.get('/fruit', function(req, res, next) {
-  res.render('fruit', { loggedIn: true });
+// process the signup form
+router.post('/sign-up', passport.authenticate('local-sign-up', {
+    successRedirect : '/home', // redirect to the secure profile section
+    failureRedirect : '/sign-up', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+}));
+
+
+// have to be logged in to visit
+router.get('/profile', isLoggedIn, function(req, res) {
+    res.render('profile', {
+        user : req.user, // get the user out of session and pass to template
+        navbar: true
+    });
 });
 
-router.get('/general-questions', function(req, res, next) {
-  res.render('general-questions', { loggedIn: true });
+
+router.get('/sign-out', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+router.get('/home', isLoggedIn, function(req, res, next) {
+  res.render('index', { navbar: true });
+});
+
+router.get('/fruit', isLoggedIn, function(req, res, next) {
+  res.render('fruit', { navbar: true });
+});
+
+router.get('/general-questions', isLoggedIn, function(req, res, next) {
+  res.render('general-questions', { navbar: true });
 });
 
 module.exports = router;
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}

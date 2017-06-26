6320be8ecbe39221e58f611920d065095a93ecb6
Luke Josten <josten_luke_j@lilly.com>
2017-06-26 16:39:36 -0400

    Made the app more pretty!

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, 'lib/public')));
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'images/brain.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
